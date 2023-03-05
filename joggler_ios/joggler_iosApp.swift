0443925c86dc109d5a7855605cc4e6b1206ce298
Jochem Vredeveld <jochem.vredeveld@gmail.com>
2023-03-05 21:27:30 +0100

    Initial Commit

---
+++ joggler_ios/joggler_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  joggler_iosApp.swift
+//  joggler_ios
+//
+//  Created by Jochem Vredeveld on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct joggler_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
